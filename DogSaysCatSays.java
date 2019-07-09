9fcf6eca99791285aa5a44c2bd01e741f2a132c9
Nicole D <52378875+Nicoled21@users.noreply.github.com>
2019-07-09 19:27:59 -0400

    Add files via upload

---
+++ DogSaysCatSays.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class IfElsePractice {
+     public static void main(String[] args){
+         Scanner input = new Scanner(System.in); 
+         String Enter = Input.nextLine("Cat or Dog");
+         System.out.println("Animal Sound");
+         int Guess = input.nextInt();
+         if (int >=1 && int<=10); {  
+         //Between 1-10
+           System.out.println("Wow!");
+                      
+         } else {
+             System.out.println("Nope!");
+         
+         
+    }
+   
+   } 
+}
